3547d3ef05ae3c35857c0c68b37cb89d934479d2
Frederick Jaime <frederickjaime@gmail.com>
2019-08-16 15:19:02 -0400

    email subscription event listerner updated logic.

--- assets/src/js/components/email.subscription.js
+++ assets/src/js/components/email.subscription.js
@@ -10,26 +10,25 @@ export default function () {
 
     let closeBtn = subscriptionOverlay.querySelector('.email-subscription-close');
 
-    document.addEventListener('click', (e) => {
+    subscriptionOverlay.addEventListener('click',(e) => {
       const flyoutElement = document.querySelector('.email-subscription-holder');
       let targetElement = e.target; // clicked element
-  
+        
       do {
-          if (targetElement == flyoutElement) {
-              // Do nothing, just return.
-              return;
-          }
-          // Go up the DOM.
+        if (targetElement == flyoutElement) {
+            // Do nothing, just return.
+            return;
+        }
+        // Go up the DOM.
           targetElement = targetElement.parentNode;
       } while (targetElement); 
-  
- 
+
       // Do something useful here.
       if(!subscriptionOverlay.classList.contains('hidden')) {
         closeBtn.click();
       }
-    });
 
+    });
 
     closeBtn.addEventListener('click', (e) => {
       subscriptionOverlay.classList.add('closed');
