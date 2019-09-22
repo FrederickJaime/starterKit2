e7d7c188c9b86a8960da4a31cc40c9ea54b96ec2
Frederick Jaime <fjaime@rauxa.com>
2019-09-22 14:46:22 -0400

    started form styless

--- assets/src/js/components/sample.component.js
+++ assets/src/js/components/sample.component.js
@@ -1,8 +1,39 @@
+import getBreakpoint from './breakpoints';
+
 export default function () {
 
 
-    console.log('you got it running!!!!');
-    // you can add all of you js here for the moment
+
+  // Moving cta button
+  // =================
+
+  // placing button and containers in variables
+  let calloutBtn = document.querySelector('.callout__cta');
+  let calloutFirst = document.querySelector('.callout > div:first-child');
+  let calloutLast = document.querySelector('.callout > div:last-child');
+
+  // creating Breakpoint to watch out for.
+  let mq = window.matchMedia(`screen and (min-width: ${getBreakpoint('md')})`);
+  var mqCheck = function( mq ) {
+    // removing btn
+    calloutBtn.remove();
+    // checking match
+    if(mq.matches) {
+      // append to first div
+      calloutFirst.appendChild(calloutBtn);
+    } else {
+      // append to last div
+      calloutLast.appendChild(calloutBtn);
+    } 
+  }
+  
+  // checks at runtime
+  mqCheck(mq);
+  
+  // attaching eventListener
+  mq.addListener(mqCheck);
+  
+
   
   }
   
